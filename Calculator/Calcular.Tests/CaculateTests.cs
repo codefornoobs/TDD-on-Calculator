38671046840fdf1f89eb6a1b5b9369f283c6c855
Diogo Cró <diogocror@gmail.com>
2017-11-15 17:21:17 +0000

    first commit

---
+++ Calculator/Calcular.Tests/CaculateTests.cs
@@ -0,0 +1,74 @@
+﻿using Calculator;
+using NUnit.Framework;
+using System;
+
+namespace Calcular.Tests
+{
+    [TestFixture]
+    public class CaculateTests
+    {
+        [Test]
+        public void CalculateSum(
+            [Values(-10, 0, 7)] int x,
+            [Values(-5, 0, 4)] int y)
+        {
+            //Arrange
+
+            //Act
+            int output = Calculate.Sum(x, y);
+            //Assert
+            Assert.AreEqual(x + y, output);
+        }
+
+        [Test]
+        public void CalculateSub(
+            [Values(-10, 0, 7)] int x,
+            [Values(-5, 0, 4)] int y)
+        {
+            //Arrange
+
+            //Act
+            int output = Calculate.Sub(x, y);
+            //Assert
+            Assert.AreEqual(x - y, output);
+        }
+
+        [Test]
+        public void CalculateDiv(
+            [Values(0, -20, 18)] int x,
+            [Values(-14, 4)] int y)
+        {
+            //Arrange
+
+            //Act
+            int output = Calculate.Div(x, y);
+            //Assert
+            Assert.AreEqual(x / y, output);
+        }
+        [Test]
+        public void CalculateDivCannotDivideZero(
+            [Values(0, -20, 18)] int x,
+            [Values(0, 0)] int y)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Throws<DivideByZeroException>(() => CalculateDiv(x, y));
+        }
+
+        [Test]
+        public void CalculateMult(
+            [Values(-10, 0, 7)] int x,
+            [Values(-5, 0, 4)] int y)
+        {
+            //Arrange
+
+            //Act
+            int output = Calculate.Mult(x, y);
+            //Assert
+            Assert.AreEqual(x * y, output);
+        }
+    }
+}
---
+++ Calculator/Calculator/Calculate.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Calculator
+{
+    public class Calculate
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Mult(int i, int i1)
+        {
+            return i * i1;
+        }
+
+        public static int Div(int i, int i1)
+        {
+            return i / i1;
+        }
+
+        public static int Sub(int i, int i1)
+        {
+            return i - i1;
+        }
+    }
+}
